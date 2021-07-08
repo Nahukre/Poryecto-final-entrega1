6c0bd9f1fa16388ab383936374788ec3165f3ebd
Nahukre <79340552+Nahukre@users.noreply.github.com>
2021-07-08 00:12:37 -0300

    Cambios

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -18,17 +18,18 @@ const porciento = x => x * 0.01;
 const division = (a, b) => a / b;
 const suma = (a, b) => a + b;
 
-
 let nombre = prompt("Cuál es su nombre?");
 let mail = prompt("Ingrese su mail");
+let objetivo = prompt("Qué bien desea alcanzar con sus ahorros?");
 let sueldo = parseInt(prompt("Indique cuál es su sueldo"));
 let ahorro = parseInt(prompt("indique cuánto ahorra al mes"));
 let extra = parseInt(prompt("Si tiene algún ingreso extra destinado a ahorros en el año indique la suma. Caso contrario ponga 0."))
-let objetivo = prompt("Qué bien desea alcanzar con sus ahorros?");
 let valorBien = parseInt(prompt("Cuál es el valor del bien que desea alcanzar con sus ahorros?"));
 
+const persona1 = new persona(nombre, mail, sueldo, ahorro, extra, objetivo, valorBien);
+console.log(persona1.toString());
 
-let ahorroPorcentaje = porciento(multiplicacion(sueldo, ahorro));
+let ahorroPorcentaje = division((multiplicacion(100, ahorro)), sueldo);
 let ahorroAnual = suma((multiplicacion(ahorro, 12)), extra);
 let mesesDeAhorro = division(valorBien, sueldo);
 let ahorroReal = division(valorBien, ahorro);
@@ -37,51 +38,30 @@ let ahorroAños = division(ahorroReal, 12);
 let tiempoTotal = division(valorBien, ahorroAnual);
 let tiempoPlus = division(valorBien, multiplicacion(ahorroAnual, 1.1));
 
-const persona1 = new persona(nombre, mail, sueldo, ahorro, extra, objetivo, valorBien);
-console.log(persona1.toString());
-
-class Inversion {
-    constructor(denominacion, moneda, valor, descripcion) {
-        this.denominacion = denominacion;
-        this.moneda = moneda;
-        this.valor = valor;
-        this.descripcion = descripcion;
+const opcionesDeInversion = [
+    { nombre: "Dolar blue", valor: 170, descripcion: "moneda norteamericana en el mercado informal" },
+    { nombre: "Bitcoin", valor: 7000000, descripcion: "Criptomoneda" },
+    { nombre: "Testa cedear", valor: 7500, descripcion: "Cedear de la compañia Tesla. Activo financiero atado al dolar y a la valuación de la empresa en el mercado." },
+    { nombre: "Apple cedear", valor: 3500, descripcion: "Cedear de la compañia Tesla. Activo financiero atado al dolar y a la valuación de la empresa en el mercado." },
+    { nombre: "Disney cedear", valor: 4500, descripcion: "Cedear de la compañia Tesla. Activo financiero atado al dolar y a la valuación de la empresa en el mercado." },
+];
+opcionesDeInversion.sort((a, b) => {
+    if (a.valor < b.valor) {
+        return -1;
     }
-}
-class activoDeInversion {
-    constructor(origen) {
-        this.origen = origen;
-        this.activo = [];
+    if (a.valor > b.valor) {
+        return 1;
     }
-    agregarInversion(inversion) {
-        this.activo.push(inversion);
-    }
-}
-const nacional = new activoDeInversion("Activos dentro del mercado local");
-const dolarBlue = new Inversion("Dolar blue", "peso", 170, "moneda norteamericana en el mercado informal");
-const bitcoin = new Inversion("Bitcoin", "dolar", 35000, "Criptomoneda");
-const cedearTesla = new Inversion("Testa cedear", "peso", 7500, "Cedears de la compañia Tesla. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
-const cedearApple = new Inversion("Apple cedear", "peso", 3500, "Cedears de la compañia Apple. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
-const cedearAmazon = new Inversion("Amazon cedear", "peso", 4500, "Cedears de la compañia Amazon. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
+    return 0;
+});
 
-nacional.agregarInversion(dolarBlue);
-nacional.agregarInversion(bitcoin);
-nacional.agregarInversion(cedearTesla);
-nacional.agregarInversion(cedearApple);
-nacional.agregarInversion(cedearAmazon);
 
-console.log("Sus opciones de inversión son:");
-console.log(dolarBlue);
-console.log(bitcoin);
-console.log(cedearAmazon);
-console.log(cedearApple);
-console.log(cedearTesla);
+let dolarBlueAhorro = division(ahorro, 170);
+let bitcoinAhorro = division(ahorro, 7000000);
+let appleAhorro = division(ahorro, 3500);
+let amazonAhorro = division(ahorro, 4500);
+let teslaAhorro = division(ahorro, 7500);
 
-console.log(dolarBlue.toString());
-console.log(bitcoin.toString());
-console.log(cedearAmazon.toString());
-console.log(cedearApple.toString());
-console.log(cedearTesla.toString());
 
 switch (true) {
     case (ahorroPorcentaje <= 5):
@@ -104,22 +84,16 @@ switch (true) {
         break;
 }
 
-
 console.log("Su porcentaje de ahorro mensual es del " + ahorroPorcentaje + "%.");
 console.log("Su ahorro anual es de $" + ahorroAnual + ".");
 console.log("Si destinara todo su sueldo a comprar su " + objetivo + " tardaría " + mesesDeAhorro + " meses o " + sueldoAños + " años para comprar lo que desea.");
 console.log("Si destinara solo su ahorro mensual a comprar su " + objetivo + " tardaría " + ahorroReal + " meses o " + ahorroAños + " años para comprar lo que desea.");
 console.log("Si destinara su ahorro mensual más sus ingresos extra anuales a comprar su " + objetivo + " tardaría " + tiempoTotal + " años para comprar lo que desea.");
 console.log("Si consiguiera aumentar su ahorro anual un 10 % para adquirir su " + objetivo + " tardaría " + tiempoPlus + " años para comprar lo que desea.");
-
-let dolarBlueAhorro = division(ahorro, 175);
-let bitcoinAhorro = division(ahorro, 6900000);
-let appleAhorro = division(ahorro, 3500);
-let amazonAhorro = division(ahorro, 4500);
-let teslaAhorro = division(ahorro, 7500);
-
 console.log("Usted podría comprar con su ahorro mensual " + dolarBlueAhorro + " dólares blue.");
 console.log("Usted podría comprar con su ahorro mensual " + bitcoinAhorro + " bitcoins.");
 console.log("Usted podría comprar con su ahorro mensual " + appleAhorro + " cedears de Apple.");
 console.log("Usted podría comprar con su ahorro mensual " + amazonAhorro + " cedears de Amazon.");
 console.log("Usted podría comprar con su ahorro mensual " + teslaAhorro + " cedears de Tesla.");
+console.log("Sus opciones de inversión inmediata son:");
+console.log(opcionesDeInversion);
